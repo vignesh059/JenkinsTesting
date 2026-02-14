e85f7851faabee73ef08b337907924edeac1feef
vignesh059 <vijjapuvignesh@gmail.com>
2026-02-14 15:08:49 +0530

    Third commit

---
+++ src/main/java/cse/JenkinsTesting/cseRocks.java
@@ -0,0 +1,15 @@
+package cse.JenkinsTesting;
+
+public class cseRocks {
+	public int add(int a, int b)
+	{
+		return a+b;
+	}
+	public int mul(int a, int b)
+	{
+		return a*b;
+	}
+	
+	
+
+}
---
+++ src/test/java/cse/JenkinsTesting/cseRocksTest.java
@@ -0,0 +1,18 @@
+package cse.JenkinsTesting;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class cseRocksTest {
+	cseRocks r = new cseRocks();
+	@Test
+	void test1() {
+		int res = r.add(2, 3);
+		assertEquals(5,res);
+	}
+	void test2() {
+		int res = r.mul(2, 3);
+		assertEquals(6,res);
+	}
+}
